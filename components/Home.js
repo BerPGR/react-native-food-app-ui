29b6eb2d81b8eac21dbf9af15f63fa82b3e00e0f
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-09-12 19:18:56 -0300

    Header styling

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Image,  View, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Feather from 'react-native-vector-icons/Feather'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import categoriesData from '../assets/data/categoriesData';
+import popularData from '../assets/data/popularData';
+import colors from '../assets/colors/colors';
+
+Feather.loadFont()
 
 export default Home = () => {
     return (
-        <Text>Home Page</Text>
+        <View style={styles.container}>
+            {/* Header */}
+            <SafeAreaView>
+                <View style={styles.headerWrapper}>
+                    <Image source={require('../assets/images/profile.png')} style={styles.profileImage}/>
+                    <Feather name="menu" size={24} color={colors.textDark}></Feather>
+                    {/* Icon */}
+                </View>
+            </SafeAreaView>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingTop: 20,
+        alignItems: 'center'
+    },
+
+    profileImage: {
+        width: 40,
+        height: 40,
+        borderRadius: 40,
+    }
+})

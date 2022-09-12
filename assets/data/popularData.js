5002ebd7a07228830dfabdf2d33f783f31851434
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-09-12 18:38:47 -0300

    Setting up React Navigation and data files

---
+++ assets/data/popularData.js
@@ -0,0 +1,25 @@
+const popularData = [
+    {
+        id: 1,
+        image: require('../images/pizza1.png'),
+        title: 'Primavera Pizza',
+        weight: '540 gr',
+        rating: '5.0'
+    },
+    {
+        id: 2,
+        image: require('../images/pizza1.png'),
+        title: 'Vegeterian Pizza',
+        weight: '450 gr',
+        rating: '4.5'
+    },
+    {
+        id: 3,
+        image: require('../images/pizza1.png'),
+        title: 'Pepperoni Pizza',
+        weight: '700 gr',
+        rating: '5.0'
+    }
+]
+
+export default popularData;

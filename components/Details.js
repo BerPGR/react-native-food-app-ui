c72509979678e66dde2592dde0f64f707834d15d
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-09-13 19:27:46 -0300

    Details header styling

--- components/Details.js
+++ components/Details.js
@@ -1,12 +1,61 @@
 import React from 'react';
-import { Text,  View } from 'react-native';
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Feather from 'react-native-vector-icons/Feather'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import colors from '../assets/colors/colors';
 
-export default Details = ({ route }) => {
+Feather.loadFont()
+MaterialCommunityIcons.loadFont()
+
+export default Details = ({ route, navigation }) => {
 
     const { item } = route.params;
     return (
-        <View>
-            <Text>Details page</Text>
+        <View style={styles.container}>
+            {/* Header */}
+            <SafeAreaView>
+                <View style={styles.headerWrapper}>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <View style={styles.headerLeft}>
+                            <Feather name="chevron-left" size={12} color={colors.textDark}/>
+                        </View>
+                    </TouchableOpacity>
+                    <View style={styles.headerRight}>
+                        <MaterialCommunityIcons name="star" size={12} color={colors.white}/>
+                    </View>
+                </View>
+            </SafeAreaView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,    
+    },
+
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingTop: 20
+    },
+
+    headerLeft: {
+        borderColor: colors.textLight,
+        borderWidth: 2,
+        padding: 12,
+        borderRadius: 10
+    },
+
+    headerRight: {
+        backgroundColor: colors.primary,
+        padding: 12,
+        borderRadius: 10,
+        borderColor: colors.primary,
+        borderWidth: 2,
+    },
+
+})

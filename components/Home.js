2e5db3064694c6a3d7c3cb6f69f8ec621a3785af
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-09-13 14:56:22 -0300

    Categories FlatList

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Image,  View, StyleSheet } from 'react-native';
+import { Text, Image,  View, StyleSheet, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Feather from 'react-native-vector-icons/Feather'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
@@ -10,6 +10,29 @@ import colors from '../assets/colors/colors';
 Feather.loadFont()
 
 export default Home = () => {
+
+    const renderCategoryItem = ({item}) => {
+        return (
+            <View style={[styles.categoryItemWrapper, {
+                backgroundColor: item.selected ? colors.primary : colors.white,
+                marginLeft: item.id == 1 ? 20 : 0
+            }]}>
+                <Image source={item.image} style={styles.categoryItemImage}/>
+                <Text style={styles.categoryItemTitle}>{item.title}</Text>
+                <View style={[styles.categorySelectWrapper, {
+                    backgroundColor: item.selected ? colors.white : colors.secondary 
+                }]}>
+                    <Feather 
+                        name="chevron-right" 
+                        size={12} 
+                        style={styles.categorySelectIcon}
+                        color={item.selected ? colors.black : colors.white}    
+                    />
+                </View>
+            </View>
+        )
+    }
+
     return (
         <View style={styles.container}>
             {/* Header */}
@@ -32,6 +55,19 @@ export default Home = () => {
                 <Feather name="search" size={16} color={colors.textDark}/>
                 <View style={styles.search}>
                     <Text style={styles.searchText}>Search</Text>
+                </View>
+            </View>
+
+            {/* Categories */}
+            <View style={styles.categoriesWrapper}>
+                <Text style={styles.categoriesTitle}>Categories</Text>
+                <View style={styles.categoriesListWrapper}>
+                    <FlatList 
+                        data={categoriesData}
+                        renderItem={renderCategoryItem}
+                        keyExtractor={item => item.id}
+                        horizontal={true}
+                    />
                 </View>
             </View>
         </View>
@@ -96,4 +132,53 @@ const styles = StyleSheet.create({
         color: colors.textLight
     },
 
+    categoriesWrapper: {
+        marginTop: 30,
+    },
+
+    categoriesTitle: {
+        fontFamily: 'Montserrat-Bold',
+        fontSize: 16,
+        paddingHorizontal: 20,
+    },
+
+    categoriesListWrapper: {
+        paddingTop: 15,
+        paddingBottom: 20 
+    },
+
+    categoryItemWrapper: {
+        backgroundColor: '#F5CA48',
+        marginRight: 20,
+        borderRadius: 20
+    },
+
+    categoryItemImage: {
+        width: 60,
+        height: 60,
+        marginTop: 24,
+        alignSelf: 'center',
+        marginHorizontal: 20
+    },
+
+    categoryItemTitle: {
+        textAlign: 'center',
+        fontFamily: 'Montserrat-SemiBold',
+        fontSize: 14,
+        marginTop: 10
+    },
+
+    categorySelectWrapper: {
+        alignSelf: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+        width: 26,
+        height: 26,
+        borderRadius: 26,
+        marginBottom: 20
+    },
+
+    categorySelectIcon: {
+        alignSelf: 'center'
+    },
 })
